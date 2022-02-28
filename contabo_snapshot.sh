7db12f8f02dbe24b385f1634f17bc9396864deeb
Paolo Finardi <pa.finardi@gmail.com>
2022-02-28 11:04:16 +0100

    Improved instances JSON parsing

--- contabo_snapshot.sh
+++ contabo_snapshot.sh
@@ -12,7 +12,7 @@ source $SCRIPT_DIR/config.conf
 ACCESS_TOKEN=$(curl -d "client_id=$CLIENT_ID" -d "client_secret=$CLIENT_SECRET" --data-urlencode "username=$API_USER" --data-urlencode "password=$API_PASSWORD" -d 'grant_type=password' 'https://auth.contabo.com/auth/realms/contabo/protocol/openid-connect/token' | jq -r '.access_token')
 
 # get instances
-instances=($(curl -X GET 'https://api.contabo.com/v1/compute/instances' -H 'Content-Type: application/json' -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-request-id: 51A87ECD-754E-4104-9C54-D01AD0F83406" -H 'x-trace-id: 123213' | jq -r '.data' | jq -r '.[].instanceId'))
+instances=($(curl -X GET 'https://api.contabo.com/v1/compute/instances' -H 'Content-Type: application/json' -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-request-id: 51A87ECD-754E-4104-9C54-D01AD0F83406" -H 'x-trace-id: 123213' | jq -r '.data[]|{instanceId}[]'))
 
 
 # for each instance, delete the oldest snapshot and create new snapshot named with timestamp
