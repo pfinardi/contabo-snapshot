1fa2ed0199c6385f30cd224104f95ce07d53fd8a
Paolo Finardi <pa.finardi@gmail.com>
2022-02-28 12:46:17 +0100

    made shellcheck happy

--- contabo_snapshot.sh
+++ contabo_snapshot.sh
@@ -6,7 +6,7 @@ timestamp=$(date +%Y%m%d-%H%M%S)
 
 # Load config values
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source $SCRIPT_DIR/config.conf
+source "$SCRIPT_DIR/config.conf"
 
 # get TOKEN
 ACCESS_TOKEN=$(curl -d "client_id=$CLIENT_ID" -d "client_secret=$CLIENT_SECRET" --data-urlencode "username=$API_USER" --data-urlencode "password=$API_PASSWORD" -d 'grant_type=password' 'https://auth.contabo.com/auth/realms/contabo/protocol/openid-connect/token' | jq -r '.access_token')
@@ -16,7 +16,7 @@ TRACE_ID=$(uuidgen)
 
 # get instances
 UUID=$(uuidgen)
-instances=($(curl -X GET 'https://api.contabo.com/v1/compute/instances' -H 'Content-Type: application/json' -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-request-id: ${UUID}" -H "x-trace-id: ${TRACE_ID}" | jq -r '.data[]|{instanceId}[]'))
+instances=($(curl -X GET "https://api.contabo.com/v1/compute/instances" -H 'Content-Type: application/json' -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-request-id: ${UUID}" -H "x-trace-id: ${TRACE_ID}" | jq -r '.data[]|{instanceId}[]'))
 
 # for each instance, delete the oldest snapshot and create new snapshot named with timestamp
 for i in "${instances[@]}"; do
