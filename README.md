b1096743a039c8adc65542b6d5ca4a7fb650cbba
Paolo Finardi <pa.finardi@gmail.com>
2022-02-28 08:28:44 +0100

    First commit!

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# contabo-snapshot
-Bash script to snaphot Contabo VPS using Contabo API
+# Contabo VPS Snapshot
+
+Simple bash script to snapshot your [Contabo VPS](https://contabo.com/en/vps/) using [Contabo API](https://api.contabo.com/).
+
+To use it:
+
+- `git clone`
+- copy `config.conf.template` to `config.conf`
+- put your Contabo credentials in `config.conf` 
+- excecute the script: `./contabo_snapshot.sh`
+- that's all
+
+The script delete the oldest snapshot and create a new snapshot.
---
+++ contabo_snapshot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+declare -a instances
+
+timestamp=$(date +%Y%m%d-%H%M%S)
+
+# Load config values
+source config.conf
+
+# get TOKEN
+ACCESS_TOKEN=$(curl -d "client_id=$CLIENT_ID" -d "client_secret=$CLIENT_SECRET" --data-urlencode "username=$API_USER" --data-urlencode "password=$API_PASSWORD" -d 'grant_type=password' 'https://auth.contabo.com/auth/realms/contabo/protocol/openid-connect/token' | jq -r '.access_token')
+
+# get instances
+instances=($(curl -X GET 'https://api.contabo.com/v1/compute/instances' -H 'Content-Type: application/json' -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "x-request-id: 51A87ECD-754E-4104-9C54-D01AD0F83406" -H 'x-trace-id: 123213' | jq -r '.data' | jq -r '.[].instanceId'))
+
+
+# for each instance, delete the oldest snapshot and create new snapsho named with timestamp
+for i in "${instances[@]}"; do
+  FIRST_SNAPSHOT=$(curl -X GET 'https://api.contabo.com/v1/compute/instances/'$i'/snapshots' -H 'Content-Type: application/json' -H "Authorization: Bearer ${ACCESS_TOKEN}" -H 'x-request-id: 04e0f898-37b4-48bc-a794-1a57abe6aa31' -H 'x-trace-id: 123213' |jq '.data'| jq -r '.[0].snapshotId')
+  curl -X DELETE 'https://api.contabo.com/v1/compute/instances/'$i'/snapshots/'$FIRST_SNAPSHOT -H 'Content-Type: application/json' -H "Authorization: Bearer ${ACCESS_TOKEN}" -H 'x-request-id: 04e0f898-37b4-48bc-a794-1a57abe6aa31' -H 'x-trace-id: 123213'
+  curl -X POST 'https://api.contabo.com/v1/compute/instances/'$i'/snapshots' -H 'Content-Type: application/json' -H "Authorization: Bearer ${ACCESS_TOKEN}" -H 'x-request-id: 04e0f898-37b4-48bc-a794-1a57abe6aa31' -H 'x-trace-id: 123213' -d '{"name":"'"$timestamp"'","description":"Snapshot-Description"}'
+done
+
+exit 0
